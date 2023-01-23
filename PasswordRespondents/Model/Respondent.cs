35b27d641aaff388c96563a11d3d8f265e545fad
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-23 08:49:59 +0500

    Create project .NET 6

--- PasswordRespondents/Model/Respondent.cs
+++ PasswordRespondents/Model/Respondent.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Websbor.PasswordRespondents.Models
+namespace PasswordRespondents.Model
 {
-    internal class ModelRespondents
+    internal class Respondent
     {
         private DataColumn _id;
         private DataColumn _nameResp;
@@ -17,12 +17,10 @@ namespace Websbor.PasswordRespondents.Models
         private DataColumn _userCreate;
         private DataColumn _dateUpdate;
         private DataColumn _userUpdate;
-        private DataColumn[] _modelDataColumn;
-        public DataColumn [] ModelDataColumn { get => _modelDataColumn;}        
-        public ModelRespondents()
+        private DataColumn[] _columns;
+        public Respondent()
         {
-            
-            _id = new DataColumn("id", typeof(int));           
+            _id = new DataColumn("id", typeof(int));
             _nameResp = new DataColumn("name_resp", typeof(string));
             _okpoResp = new DataColumn("okpo_resp", typeof(string));
             _passwordResp = new DataColumn("password_resp", typeof(string));
@@ -30,9 +28,17 @@ namespace Websbor.PasswordRespondents.Models
             _userCreate = new DataColumn("user_create", typeof(string));
             _dateUpdate = new DataColumn("date_update", typeof(DateTime));
             _userUpdate = new DataColumn("user_update", typeof(string));
-            
-            _modelDataColumn = new DataColumn[8] { _id, _nameResp,_okpoResp, _passwordResp, _dateCreate, _userCreate,
-            _dateUpdate, _userUpdate};            
+
+            _columns = new DataColumn[8] { _id, _nameResp,_okpoResp, _passwordResp, _dateCreate, _userCreate,
+            _dateUpdate, _userUpdate};
+        }
+
+        public DataTable GetDataTableRespondent()
+        {
+            DataTable dataTableRespondents = new();
+            dataTableRespondents.Columns.AddRange(_columns);
+
+            return dataTableRespondents;
         }
     }
 }
